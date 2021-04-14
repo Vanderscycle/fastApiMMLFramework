8f49e04a5d5bf76a9373fd50ea2bab0ab85ef6c4
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-04-14 14:25:43 -0700

    synchronization with docker-compose and the init file for postgres.

--- postgresSQL/init.sql
+++ postgresSQL/init.sql
@@ -1,16 +1,14 @@
 CREATE DATABASE testApi;
 
 -- do we mean first name and amily name?
-CREATE TABLE IF NOT EXISTS userDescription (
-    ID serial PRIMARY KEY,
-    Name VARCHAR (50) NOT NULL,
-    Family VARCHAR (50) NOT NULL,
-    Description TEXT, 
-);
+CREATE TABLE IF NOT EXISTS user (
+    id serial PRIMARY KEY,
+    name VARCHAR (50) NOT NULL,
+    family VARCHAR (50) NOT NULL,
+    description TEXT);
 
 CREATE USER root WITH PASSWORD 'Vand^2VictoDataEngineerRoot';
 ALTER USER root WITH SUPERUSER;
-CREATE DATABASE userDescription;
-GRANT ALL PRIVILEGES ON DATABASE userDescription TO root;
+GRANT ALL PRIVILEGES ON DATABASE testApi TO root;
 
 
