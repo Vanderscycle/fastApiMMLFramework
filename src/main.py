af3cdec799d865d754290bec430248533492d62b
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-04-14 18:13:20 -0700

    UPDATE and DELETE work. getall users not so much.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-from sqlalchemy import create_engine, Column, Integer, String
+# https://towardsdatascience.com/sqlalchemy-python-tutorial-79a577141a91
+from sqlalchemy import inspect,create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.util.langhelpers import decode_slice
 SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:test@sql-db/testApi"
 
 Base = declarative_base()
@@ -47,15 +49,63 @@ app = FastAPI()
 #GET
 @app.get("/users/{userId}", response_model=User)
 async def readUser(userId: int,db: Session = Depends(get_db)):
+    # I have no clue why userId start at 1 instead of 0
     _user = db.query(UserModel).get(userId)
-    return _user
+    inspector = inspect(engine)
+    print(inspector.get_columns('user'))
+    try:
+        return _user
+    except Exception as e:
+        return {"message": e}
+
+
+#GETALL
+@app.get("/users/",response_model=User)
+async def giveAllUser(db: Session = Depends(get_db)):
+    users = db.query(UserModel).all()
+    output = list()
+    for user in users:
+        try:
+            singleEntry = {"name":user.name,"family":user.family,"description":user.description}
+            output.append(jsonable_encoder(singleEntry))
+        except Exception as e:
+            print(e)
+    return output
+
 
 #POST
 @app.post("/users/",response_model=User)
 async def createUser(user:User,db: Session = Depends(get_db)):
     _user = UserModel(name = user.name,family =user.family,description = user.description)
     db.add(_user)
-    print(_user)
     db.commit()
     db.refresh(_user)
     return _user
+
+
+#DELETE
+@app.delete("/users/{userId}")
+async def deleteUser(userId:int,db: Session = Depends(get_db)):
+    db.query(UserModel).filter(UserModel.id==userId).delete()
+    try:
+        db.commit()
+        return {"message": f"userId {userId} delete"}
+    except Exception as e:
+        return {"message": e}
+
+
+# PATCH
+@app.patch("/users/{userId}",response_model=User)
+async def patchUser(userId:int, replacementData: User, db: Session = Depends(get_db)):
+    _user = db.query(UserModel).get(userId)
+    updateData = replacementData.dict()
+    dictUser = {"id":_user.id,"name":_user.name,"family":_user.family,"description":_user.description}
+
+    # need to find a way to use the default dtype to compare
+    for k,v in updateData.items():
+        if v != 'string':
+            dictUser[k] = v
+    # not scalable, need more time with pydantic
+    db.query(UserModel).filter(UserModel.id == userId).update({UserModel.name:dictUser["name"],UserModel.family:dictUser["family"],UserModel.description:dictUser["description"]})
+    db.commit()
+    return dictUser
